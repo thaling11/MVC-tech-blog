6ff10b88a07fa4aba55b2f16094ae54d4a228a42
thaling <thaling@verizon.net>
2022-10-01 14:46:20 -0600

    complete user-routes and index.js routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,53 +1,107 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
-const authorize = require('../../utils/auth');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const authorize = require("../../utils/auth");
 
 //GET all
-router.get('/', (req, res) => {
-    User.findAll({
-        attributes: { exclude: ['password'] }
-    })
-      .then(data => res.json(data))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+router.get("/", (req, res) => {
+  User.findAll({
+    attributes: { exclude: ["password"] },
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //GET by ID
-  router.get('/:id', (req, res) => {
-    User.findOne({
-      attributes: { exclude: ['password'] },
-      where: {
-        id: req.params.id
+router.get("/:id", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Post,
+        attributes: ["id", "title", "post_text", "created_at"],
       },
-      include: [
-        {
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
           model: Post,
-          attributes: ['id', 'title', 'post_text', 'created_at']
+          attributes: ["title", "post_id"],
         },
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'created_at'],
-          include: {
-            model: Post,
-            attributes: ['title', 'post_id']
-          }
-        }
-      ]
+      },
+    ],
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(data);
     })
-      .then(data => {
-        if (!data) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(data);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
+//POST new user
+router.post("/", (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  })
+    .then((data) => {
+      req.session.save(() => {
+        req.session.user_id = data.id;
+        req.session.username = data.username;
+        req.session.loggedIn = true;
+
+        res.json(data);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST login route for user
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((data) => {
+    if (!data) {
+      res
+        .status(400)
+        .json({ message: "No user found with entered email address" });
+      return;
+    }
+    const validPassword = data.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+    res.json({ user: data, message: "Success!" });
+  });
+});
+
+// POST log out an existing user
+router.post("/logout", authorize, (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/api', apiRoutes);
+
+router.use('/', homeRoutes);
+
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
