8754d66a118cf4ce9a4fec81c42c2ab5058a2ec4
thaling <thaling@verizon.net>
2022-10-05 08:09:11 -0600

    comment routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { response } = require('express');
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//ADD WITHAUTH
+router.get('/', (req, res) => {
+    Comment.findAll()
+      .then(data => res.json(data))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  //withAuth
+  router.post('/', (req, res) => {
+    if (req.session) {
+      Comment.create({
+        title: req.body.title,
+        comment_text: req.body.comment_text,
+        user_id: req.session.user_id,
+        post_id: req.body.post_id
+      })
+        .then(data => res.json(data))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    }
+  });
+  
+  //withAuth
+  router.delete('/:id', (req, res) => {
+    Comment.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(data => {
+        if (!data) {
+          res.status(404).json({ message: 'No comment found with the id' });
+          return;
+        }
+        res.json(data);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
